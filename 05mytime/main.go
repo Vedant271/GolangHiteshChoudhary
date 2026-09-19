17bb470d0ddd5d0c58ffa7caa02bd654e30518fe
Vedant Karale <90595461+Vedant271@users.noreply.github.com>
2026-09-19 19:27:33 +0000

    Golang time

---
+++ 05mytime/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study of Golang")
+
+	currentTime := time.Now()
+	fmt.Println(currentTime)
+	fmt.Println(currentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	customTime := time.Date(2020, time.August, 12, 23, 23, 0, 0, time.UTC)
+	fmt.Println(customTime)
+	fmt.Println(customTime.Format("01-02-2006 Monday"))
+}
